42e4c897d9265a9e847deda6658707ed5ea9b256
Mando <hey@armandohimself.com>
2024-09-06 13:10:26 -0500

    Playing cat challenge

---
+++ CodingExercises/PlayingCat.java
@@ -0,0 +1,37 @@
+package CodingExercises;
+
+public class PlayingCat {
+    
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temp) {
+    boolean isCatPlaying = false;
+    
+    // if summer
+    if (summer) {
+        // if temp inclusive 25-45
+        if (temp >= 25 && temp <= 45) {
+            // cats playing
+            isCatPlaying = true;
+        }
+    } 
+    // otherise 
+    else {
+        // if temp inclusive 25-35
+        if (temp >= 25 && temp <= 35) {
+            // cat plays
+            isCatPlaying = true;
+        } else {
+            isCatPlaying = false;
+        }
+        }
+    
+    return isCatPlaying;
+    //EO method
+    }
+}
