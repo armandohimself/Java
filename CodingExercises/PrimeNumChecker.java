fa5919050d562e3651a6cdb3f8b2de7c6ee73e9c
Mando <hey@armandohimself.com>
2024-09-18 14:55:56 -0500

    Sum 3 and 5 challenge

---
+++ CodingExercises/PrimeNumChecker.java
@@ -0,0 +1,32 @@
+package CodingExercises;
+
+public class PrimeNumChecker {
+    public static boolean isPrime (int wholeNumber) {
+        int counter = 0;
+
+        if (wholeNumber <= 2) {
+            counter ++;
+            System.out.println("Our counter is: " + counter);
+            return (wholeNumber == 2);
+        }
+
+        for (int divisor = 2; divisor < wholeNumber; divisor++) {
+            counter++;
+            if (wholeNumber % divisor == 0) {
+                return false;
+            } else if (counter == 3) {
+                break;
+            }
+        }
+
+        System.out.println("Our counter is: " + counter);
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(7));
+        System.out.println(isPrime(56));
+    }
+}
---
+++ CodingExercises/Sum3And5.java
@@ -0,0 +1,21 @@
+package CodingExercises;
+
+public class Sum3And5 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int breakOutCondition = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i);
+                sum+=i;
+                breakOutCondition++;
+
+                if (breakOutCondition == 5) break;
+            }
+        }
+
+        System.out.println("Summed number is: " + sum);
+    }
+}
