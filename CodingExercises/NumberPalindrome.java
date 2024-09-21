00469e2a88fa66c70a028071cf34d5ffff127ab4
Mando <hey@armandohimself.com>
2024-09-21 16:40:50 -0500

    Palindrone no conversion, only arithemtic

--- CodingExercises/NumberPalindrome.java
+++ CodingExercises/NumberPalindrome.java
@@ -2,31 +2,27 @@ package CodingExercises;
 
 public class NumberPalindrome {
     public static void main(String[] args) {
-        isPalindrome(36063);
+        isPalindrome(1234);
     }
 
 
     public static boolean isPalindrome(int number) {
-        String numberString = String.valueOf(number);
-        char[] numberStringArr = numberString.toCharArray();
-        char[] reversedNumberStringArr = new char[numberStringArr.length - 1];
-        boolean isAPalindrome = false;
-        System.out.println("numberStringArr?" + numberStringArr.toString());
-
-        for (int i = numberStringArr.length - 1; i <= 0; i--) {
-            reversedNumberStringArr[i] = numberStringArr[i];
-            System.out.println("reversedNumberStringArr: " + reversedNumberStringArr[i]);
-            System.out.println("numberStringArr: " + numberStringArr[i]);
-        }
-
-
-
-
-        //if isPalindrome   
-            //return true;
-        // otherwise 
-            // return false
+        int reverse = 0;
+        int original = number;
+        
+        
+        while (number != 0) {
             
-        return isAPalindrome;
+            reverse = (reverse * 10) + (number % 10);
+            
+            //Loop condition
+            number = number / 10;
+        }
+        
+        System.out.println("reversed: " + reverse);
+        System.out.println("original: " + original);
+
+        return original == reverse? true : false;
+        
     }
 }
