9dc580db478ad08ebc067338804f2cf7509b1e05
Mando <hey@armandohimself.com>
2024-09-23 12:45:02 -0500

    Input calculator challenge with alternate solution

---
+++ CodingExercises/InputCalculator.java
@@ -0,0 +1,77 @@
+package CodingExercises;
+
+import java.util.Scanner;
+
+public class InputCalculator {
+
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage () {
+        boolean quitProgram = false;
+        int sum = 0;
+        int counter = 0;
+
+        // Read int numbers from keyboard input using Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        do {
+
+            String userInput = sc.nextLine();
+            // Validate the input
+            try {
+                int validNumber = Integer.parseInt(userInput);
+
+                sum += validNumber;
+
+                counter++;
+
+            } catch (NumberFormatException e) {
+                if (counter == 0) { System.out.println("SUM = " + 0 + " AVG = " + 0); break; }
+
+                System.out.println("SUM = " + sum + " AVG = " + sum/counter);
+                break;
+            }
+        } while (!quitProgram);
+    }
+}
+
+/*
+ * import java.util.Scanner;
+ 
+public class InputCalculator {
+ 
+    public static void inputThenPrintSumAndAverage() {
+        
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        long avg = 0;
+ 
+        while (true) {
+            boolean hasNextInt = scanner.hasNextInt();
+
+            if (!hasNextInt) {
+                * break the loop on invalid input
+                break;
+            }
+
+            sum += scanner.nextInt();
+            
+            count++;
+            
+            * clear buffer e.g. remove line separator
+            
+            scanner.nextLine();
+        }
+         
+        if (count > 0) {
+            avg = Math.round((double) sum / count);
+        }
+ 
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+        scanner.close();
+    }
+}
+ */
