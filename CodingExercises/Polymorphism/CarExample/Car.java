77db3d2671b848e486a133957f753c965eb26465
Mando <hey@armandohimself.com>
2024-10-07 22:52:21 -0500

    Polymorphism example

---
+++ CodingExercises/Polymorphism/CarExample/Car.java
@@ -0,0 +1,33 @@
+package CodingExercises.Polymorphism.CarExample;
+
+public class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+    
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = this.getClass().getSimpleName();
+    }
+    
+    public void startEngine() {
+        System.out.println(String.format(this.getClass().getSimpleName() + " -> startEngine()"));
+    }
+    
+    public void accelerate() {
+        System.out.println(String.format(this.getClass().getSimpleName() + " -> accelerate()"));
+    }
+    
+    public void brake() {
+        System.out.println(String.format(this.getClass().getSimpleName() + " -> brake()"));
+    }
+    
+    public int getCylinders() {
+        return this.cylinders;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+}
