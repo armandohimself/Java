1d191057ec1a9b7bf133a4f3fe68d591a02ffd61
Mando <hey@armandohimself.com>
2024-10-07 22:33:29 -0500

    Printer example

---
+++ CodingExercises/Encapsulation/PrinterExample/Printer.java
@@ -0,0 +1,31 @@
+package CodingExercises.Encapsulation.PrinterExample;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = tonerLevel;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        if (tonerLevel >= 100) tonerAmount = 0;
+        if (tonerLevel < 0) tonerLevel = 0;
+        else tonerLevel+=tonerAmount;
+
+        return tonerLevel;
+    }
+
+    public int printPages(int pages) {
+        if (duplex == true) {
+            pagesPrinted = 0;
+            pagesPrinted = (pages/2);
+            System.out.println("It's a duplex printer");
+        } else {
+            pagesPrinted+=pages;
+        }
+        return pagesPrinted;
+    }
+}
