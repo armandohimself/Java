fecb7a5a9467b329e29006d59e768668fab8c02c
Mando <hey@armandohimself.com>
2024-11-09 21:20:02 -0600

    Basics to Maven

---
+++ demo/src/test/java/com/example/AppTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+
+        // fail();
+        assertTrue(true);
+    }
+}
