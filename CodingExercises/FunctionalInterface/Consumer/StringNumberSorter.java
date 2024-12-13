51a18562fb678f35c8a8c7c743975a8c92c1ad5f
Mando <hey@armandohimself.com>
2024-12-13 13:50:25 -0600

    Added exercise to do after work

---
+++ CodingExercises/FunctionalInterface/Consumer/StringNumberSorter.java
@@ -0,0 +1,5 @@
+/*
+ * Exercise (optional)
+Given an array of numbers where every number is represented as string, 
+successfully execute a Java program that sorts these numbers in ascending and descending order, using lambdas.
+ */
