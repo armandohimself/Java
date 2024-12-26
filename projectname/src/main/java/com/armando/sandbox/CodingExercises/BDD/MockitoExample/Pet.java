ac28d11b5be6ae0aaf441d4fe458fba459cb5806
Mando <hey@armandohimself.com>
2024-12-26 08:49:29 -0600

    Mockito testing

---
+++ projectname/src/main/java/com/armando/sandbox/CodingExercises/BDD/MockitoExample/Pet.java
@@ -0,0 +1,26 @@
+package com.armando.sandbox.CodingExercises.BDD.MockitoExample;
+
+public class Pet {
+    
+    private int id;
+    private String name;
+    private int age;
+    private String species;
+    private int vetId;
+
+    // ... constructors, getters/setters omitted
+
+    public String talk() {
+        return "animal noise";
+    }
+
+    public String bow() {
+        return "the pet bows";
+    }
+    
+    public void doTrick() {
+        talk();
+        bow();
+        talk();
+    }
+}
