c6d93e764cfb48b1829941bfe0d70228b954ab21
Mando <hey@armandohimself.com>
2024-12-26 02:10:02 -0600

    Annotations in JUnit 5

---
+++ projectname/src/main/java/com/armando/sandbox/CodingExercises/TDD/MathOpsExample/MathOps.java
@@ -0,0 +1,23 @@
+package com.armando.sandbox.CodingExercises.TDD.MathOpsExample;
+
+public class MathOps {
+    public int add(int first, int second) {
+        return first + second;
+    }
+}
+
+/*
+ * Exercises (Optional)
+If not done previously, do the first two steps. If you have already done Steps 1 and 2, proceed to Step 3
+
+Step 1
+Create a MathOps class and create the following method:
+[X] add : Accept two int parameters, and return their sum.
+[] Create the TestMathOps class and demonstrate the usage of the annotations and assert methods. Run the test and show the output.
+
+Step 2
+Create test methods for subtract and divide methods before writing them in MathOps.
+
+Step 3
+Create the ParameterizedMathTest class and explain the RunWith and Parameter annotations. Build the class and its test method.
+ */
